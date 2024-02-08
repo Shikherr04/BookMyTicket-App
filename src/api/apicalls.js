488d43110828ec1e4fa1d92a7d8db61534b4ff45
Shikhar <shikher.2003@gmail.com>
2024-02-08 14:24:06 +0530

    first commit

---
+++ src/api/apicalls.js
@@ -0,0 +1,16 @@
+const apikey = '6581434f3f7fb8b53dc076ea44f27050';
+export const baseImagePath = (size, path) => {
+  return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+export const nowPlayingMovies = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}`;
+export const upcomingMovies = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apikey}`;
+export const popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}`;
+export const searchMovies = (keyword) => {
+  return `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}`;
+};
+export const movieDetails = (id) => {
+  return `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}`;
+};
+export const movieCastDetails = (id) => {
+  return `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apikey}`;
+};
